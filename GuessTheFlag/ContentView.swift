d9657e053e0a72503be0a0595d8fb6b7875d401c
Jazeps Ivulis <jazeps.ivulis@accenture.com>
2023-08-22 14:10:06 +0300

    Implemented examples from Day 20 lessons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .cyan, .red]), center: .center)
+            
+            VStack(spacing: 20) {
+                Text("Hello, world!")
+                
+                HStack(spacing: 20) {
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                }
+                
+                ZStack {
+                    Text("Your content")
+                        .foregroundStyle(.secondary)
+                        .padding(5)
+                        .background(.ultraThinMaterial)
+                }
+                .frame(width: 200, height: 50)
+                .background(.red)
+                
+                Group {
+                    Button("Delete", role: .destructive, action: executeDelete)
+                    Button("Button 1") {}
+                        .buttonStyle(.bordered)
+                    Button("Button 2", role: .destructive) {}
+                        .buttonStyle(.bordered)
+                    Button("Button 3") {}
+                        .buttonStyle(.borderedProminent)
+                    Button("Button 4", role: .destructive) {}
+                        .buttonStyle(.borderedProminent)
+                    Button("Button 5") {}
+                        .buttonStyle(.borderedProminent)
+                        .tint(.mint)
+                    Button {
+                        print("Button was tapped!")
+                    } label: {
+                        Text("Tap me!")
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(.red)
+                    }
+                    Button {
+                        print("Edit button was tapped!")
+                    } label: {
+                        Label("Edit", systemImage: "pencil")
+                    }
+                    
+                    Image(systemName: "pencil")
+                        .renderingMode(.original)
+                    
+                    Button("Show alert") {
+                        showingAlert = true
+                    }
+                    .alert("Important message", isPresented: $showingAlert) {
+                        Button("Delete", role: .destructive) { }
+                        Button("Cancel", role: .cancel) { }
+                    } message: {
+                        Text("Please read this")
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    
+    func executeDelete() {
+        print("Now deleting")
     }
 }
 
